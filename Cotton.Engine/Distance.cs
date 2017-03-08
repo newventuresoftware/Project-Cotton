a354ca2f8ecdda3aac89d2d5a49ce073997e6e50
deniskyashif <denis.kyashif@gmail.com>
2017-03-08 14:14:15 +0200

    included the Levenshtein distance algorithm and added a few tests

---
+++ Cotton.Engine/Distance.cs
@@ -0,0 +1,41 @@
+﻿namespace Cotton.Engine
+{
+    using System;
+
+    public class Distance
+    {
+        public static double Levenshtein(string source, string target)
+        {
+            if (source.Length == 0)
+                return target.Length;
+
+            if (target.Length == 0)
+                return source.Length;
+
+            if (source == target)
+                return 0;
+            
+            var memo = new int[target.Length];
+
+            for (int k = 0; k < target.Length; k++)
+                memo[k] = k;
+
+            for (int i = 1; i < source.Length; i++)
+            {
+                var currentRowMemo = new int[target.Length];
+                currentRowMemo[0] = i;
+
+                for (int k = 1; k < target.Length; k++)
+                {
+                    currentRowMemo[k] = Math.Min(
+                        Math.Min(currentRowMemo[k - 1] + 1, memo[k] + 1),
+                        memo[k - 1] + (source[i - 1] != target[k - 1] ? 1 : 0));
+                }
+
+                memo = currentRowMemo;
+            }
+
+            return memo[target.Length - 1];
+        }
+    }
+}
---
+++ Cotton.Test/LevenshteinDistanceTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Cotton.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Cotton.Engine;
+
+    [TestClass]
+    public class LevenshteinDistanceTests
+    {
+        [TestMethod]
+        public void CallingLevenshtein_WithEmptyStringToEmptyString_ShouldReturn0()
+        {
+            var expected = 0;
+            var actual = Distance.Levenshtein(string.Empty, string.Empty);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CallingLevenshtein_WithEmptyStringToAbc_ShouldReturn3()
+        {
+            var expected = 3;
+            var actual = Distance.Levenshtein(string.Empty, "abc");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CallingLevenshtein_WithAbcToEmptyString_ShouldReturn3()
+        {
+            var expected = 3;
+            var actual = Distance.Levenshtein("abc", string.Empty);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CallingLevenshtein_WithHomeToHome_ShouldReturn0()
+        {
+            var expected = 0;
+            var actual = Distance.Levenshtein("home", "home");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
